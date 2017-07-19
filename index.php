8949ddb0bb0286526934586ee521b7614794efe4
Bruno Gastadi <gramado.f@gmail.com>
2017-07-19 11:15:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ function sendMessage($parameters) {
 /*
  * PEGANDO A REQUISIÇÃO
  */
+
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 if (isset($update["result"]["action"])) {
